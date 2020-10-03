59e7a8e521344b8a6eae686b4ef7a165f835babc
Juan Daniel <juanda554242@gmail.com>
2020-10-03 13:52:42 -0500

    creando clase main

---
+++ universidad/primero/src/main/java/com/jdvpl/primero/main.java
@@ -0,0 +1,14 @@
+
+package com.jdvpl.primero;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner hola=new Scanner(System.in);
+        System.out.println("Ingrese su nombre perra: ");
+        String nombre=hola.nextLine();
+        System.out.println("Su nombre es: "+nombre);
+    }
+    
+}
---
+++ universidad/primero/src/main/java/com/jdvpl/primero/persona.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jdvpl.primero;
+
+/**
+ *
+ * @author juand
+ */
+public class persona {
+    private String Codigo;
+    private String Nombre;
+    private String telefono;
+    private int edad;
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
