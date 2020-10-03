5877660c60f5afa7716974e761e19422081eada2
Juan Daniel <juanda554242@gmail.com>
2020-10-03 14:12:55 -0500

    creando las clases y obteniendola en la clase principal

---
+++ universidad/primero/src/main/java/com/jdvpl/primero/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jdvpl.primero;
+
+/**
+ *
+ * @author juand
+ */
+public class Persona {
+    public String Codigo;
+    public String Nombre;
+    public String Telefono;
+    public int Edad;
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+    
+}
