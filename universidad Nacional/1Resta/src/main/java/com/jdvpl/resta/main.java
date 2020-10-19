c2a71fa73a8572a05884b537696fc7d0a7e490fa
Juan Daniel <juanda554242@gmail.com>
2020-10-19 17:07:58 -0500

    primeros pasos con Java Nacional

---
+++ universidad Nacional/1Resta/src/main/java/com/jdvpl/resta/main.java
@@ -0,0 +1,15 @@
+
+package com.jdvpl.resta;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=Integer.parseInt(scanner.nextLine());
+        int b=Integer.parseInt(scanner.nextLine());
+        int res=a-b;
+        System.out.println(res);
+    }
+    
+}
