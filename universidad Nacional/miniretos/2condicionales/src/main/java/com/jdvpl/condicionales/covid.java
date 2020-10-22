812e77e231cbd2105a2ca861fb4fc2750e041973
Juan Daniel <juanda554242@gmail.com>
2020-10-21 21:07:31 -0500

    minireto 3 del divoc

---
+++ universidad Nacional/miniretos/2condicionales/src/main/java/com/jdvpl/condicionales/covid.java
@@ -0,0 +1,48 @@
+
+package com.jdvpl.condicionales;
+
+import java.util.Scanner;
+
+
+public class covid {
+    public static int Covid(double num, double num2){
+        
+        while (true){
+            try {
+                if(num<=100000000 && num>0){
+                break;
+            }  
+            } catch (Exception e) {
+                System.out.println("El valor es incorrecto, debes ingresar un numero");
+            }
+        }
+        while (true){
+            try {
+
+                if(num>=num2 && num2>=0){
+                break;
+            }  
+            } catch (Exception e) {
+                System.out.println("El valor es incorrecto, debes ingresar un numero"); 
+            }
+        }
+        int dia=1;
+        while (num2<num){
+            num=num*1.02;
+            num2=num2*1.03;
+            dia++;
+        }
+        return  dia;
+    }
+         
+    
+        
+   
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double num1=sc.nextDouble();
+        double num2=sc.nextDouble();
+        System.out.println(Covid(num1, num2));
+    }
+    
+}
